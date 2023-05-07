77a3844684a391587b14cd2845cdc23217fabd55
jumaniyozov <jumaniyozovse@gmail.com>
2023-05-07 19:44:07 +0500

    files emedded

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jumaniyozov/gdo/controllers"
+	"github.com/jumaniyozov/gdo/templates"
+	"github.com/jumaniyozov/gdo/views"
 )
 
 type Address struct {
@@ -22,60 +22,21 @@ type User struct {
 	Address Address
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	user := User{
-		Name: "Islom",
-		Address: Address{
-			Street:       "Kamarniso",
-			Country:      "Uzbekistan",
-			StreetNumber: 6,
-		},
-	}
-	tplPath := filepath.Join("templates", "hello.gohtml")
-	executeTemplate(w, tplPath, user)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tplPath, nil)
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "faq.gohtml")
-	executeTemplate(w, tplPath, nil)
-}
-
 func main() {
 	PORT := "8001"
-
 	r := chi.NewRouter()
-
 	r.Use(middleware.Logger)
 
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "hello.gohtml"))))
+
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
+
+	r.Get("/faq", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
 	fmt.Printf("Server is running on PORT %s...\n", PORT)
 
 	err := http.ListenAndServe(fmt.Sprintf(":%s", PORT), r)
 	if err != nil {
 		panic(err)
-	}
-}
-
-func executeTemplate(w http.ResponseWriter, filePath string, fields any) {
-	t, err := template.ParseFiles(filePath)
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
-		return
-	}
-
-	err = t.Execute(w, fields)
-	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
-		return
 	}
 }
