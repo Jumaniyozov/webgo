69acbc7f89feb85e620878fb2e8ec9a2fc3bd981
jumaniyozov <jumaniyozovse@gmail.com>
2023-05-10 01:22:06 +0500

    changes added

---
+++ cmd/exp/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	//return fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Database)
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
+
+func main() {
+	cfg := PostgresConfig{
+		Host:     "localhost",
+		Port:     "5433",
+		User:     "baloo",
+		Password: "junglebook",
+		Database: "webgo",
+		SSLMode:  "disable",
+	}
+
+	dsl := cfg.String()
+
+	db, err := sql.Open("pgx", dsl)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			id SERIAL PRIMARY KEY,
+			name TEXT,
+			email TEXT UNIQUE NOT NULL
+		);
+		
+		CREATE TABLE IF NOT EXISTS orders (
+			id SERIAL PRIMARY KEY,
+			user_Id INT NOT NULL,
+			amount INT,
+			description TEXT
+		);
+	`)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Tables created.")
+
+	name := "John Calhoun"
+	email := "john@calhoun.io"
+	_, err = db.Exec(`
+		INSERT INTO users (name,email) 
+		VALUES ($1, $2);`, name, email)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("User created")
+}
